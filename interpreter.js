c13f5d2f56fcde9c675bc7b0bfe79519e2054413
Peter Clayton <friedwhoman@gmail.com>
2021-03-31 09:34:59 -0400

    Added basic REPL and ep constants.

--- interpreter.js
+++ interpreter.js
@@ -3,6 +3,11 @@ YAPL: Yet Another Programming Language
 Made for fun.
 This is the statement interpreter.
 */
-module.exports = function interpretStatement(statement){
-    
+const cls = require("./classes")
+/**
+ * 
+ * @param {String} statement The code to be run.
+ * @param {Env} env The environment to use.
+ */
+module.exports = function interpretStatement(statement, env, ep){
 }
--- repl.js
+++ repl.js
@@ -5,6 +5,13 @@ This is the repl.
 */
 const readline = require("readline")
 const interpretStatement = require("./interpreter")
-module.exports = function startRepl(){
-    
+const cls = require("./classes")
+const say = console.log
+module.exports = function startRepl(ep){
+    var inf = readline.createInterface(process.stdin, process.stdout)
+    var env = new cls.Env()
+    say("YAPL", ep.verboseVersion, "REPL intialized.")
+    inf.on("line", (ln)=>{
+        interpretStatement(ln, env, ep)
+    })
 }
