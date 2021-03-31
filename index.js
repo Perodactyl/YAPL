50a1daa478e773c58d9a1d4dec4ee22a95504a1f
Peter Clayton <friedwhoman@gmail.com>
2021-03-31 15:37:30 -0400

    Fixed ep object

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ Made for fun.
 This is the entry point.
 */
 const say = console.log
-const ep = { // Constants that other programs might use, ep standing for entry point.
+var ep = { // Constants that other programs might use, ep standing for entry point.
     version:[0, 1, 0, true],
-    verboseVersion:ep.version.slice(0, 3).join(".")+`${version[3] ? " DEVEL" : ""}`
 }
+ep.verboseVersion = ep.version.slice(0, 3).join(".")+`${ep.version[3] ? " DEVEL" : ""}`
 var args = process.argv
 args.splice(0, 2) // Remove running the program. In a release to be built, this would splice 1 instead of 2.
 if(!args.length){
