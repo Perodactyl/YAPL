0aa6d12dcb34ef865f888fb99f5df6eee21e3466
Peter Clayton <friedwhoman@gmail.com>
2021-04-01 16:59:18 -0400

    Added colors and patched small interpreter bugs

--- interpreter.js
+++ interpreter.js
@@ -4,52 +4,46 @@ Made for fun.
 This is the statement interpreter.
 */
 const cls = require("./classes")
+const colors = require("colors")
 /**
  * 
  * @param {String} statement The code to be run.
  * @param {Env} env The environment to use.
  */
 var nativeFuncs = { // functions that are native.
-    print(data){
-        console.log("PRINTED: ", data)
+    print(...data){
+        data.forEach((val, index)=>{
+            val = val.replace(/\"/g, "")
+            val = val.trim()
+            data[index] = val
+        })
+        console.log.apply(global, data)
     }
 }
 module.exports = function interpretStatement(statement, env, ep){
-    var replacedAny = false
-	console.log("Interpreting: ", statement);
-    statement = statement.replace(/([\w+|[0-9]+])/, (m, g1)=>{
+    statement = statement.trim()
+    if(statement == "")return
+    statement = statement.replace(/([\w+|[0-9]+])/g, (m, g1)=>{
         if(env.has(g1)){
             return env.get(g1)
-            replacedAny = true
         }else{return m}
     })
-    if(replacedAny){
-	console.log("replaced env var");
-        interpretStatement(statement, env, ep)
-        return
-    }
     var mcs = { //Match Cases.
         eqcheck: /(.*)\s*==\s*(.*)/,
-		// Function name, optional whitespace, open paren, non-commas, comma, close paren
         func: /(.+)\s*\(\s*([^)]*)\s*\)$/,
-//        func: /(.*)\s*\(\s*(([^,]+)*)\s*\)$/
-//        func: /(.*)\s*\((([^,]),)\)/
     }
     var mc //What case we matched.
     var stm = statement //Just an alias to make it less typing.
-    if(mc = stm.match(mcs.eqcheck)){
-		console.log("eqcheck");
-        
-    }else if(mc = stm.match(mcs.func)){
+    stm = stm.replace(mcs.eqcheck, (m, g1, g2)=>{
+        if(g1 == g2)return "true"
+        else return "false"
+    })
+    if(mc = stm.match(mcs.func)){
 		var funcName = mc[1]
 		var funcArgs = mc[2].split(',')
-		console.log("func args: ", funcArgs);
 		if (nativeFuncs[funcName]) {
 			nativeFuncs[funcName].apply(this, funcArgs);
-		} else {
-			console.log('Not a native function: ', funcName);
-		}
-        
+        }
     } else {
 		console.log("No match");
 	}
